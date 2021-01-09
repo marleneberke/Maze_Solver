c07e65ad4093ad90eec49d7ec30f62c8d6fdb829
mdb293 <mdb293@cornell.edu>
2021-01-08 19:44:52 -0500

    saving everything

--- Model/Attempt1/execute.jl
+++ Model/Attempt1/execute.jl
@@ -3,8 +3,8 @@ using Distributions
 
 include("../maze_generator.jl")
 include("helper_functions.jl")
-include("DLS_intersection_sensitivity.jl")
-#include("old_DLS_simple.jl")
+#include("DLS_intersection_sensitivity.jl")
+include("DLS_simple_gm.jl")
 include("inference.jl")
 
 #################################################################################
@@ -15,7 +15,7 @@ w = 10
 m = maze(h,w);
 printmaze(m);
 
-Random.seed!(1);
+Random.seed!(3);
 
 #################################################################################
 node_matrix = Matrix{Node}(undef, h, w)
@@ -33,18 +33,18 @@ goal_location = node_matrix[h, w].location
 #################################################################################
 T = 30
 (trace, _) = Gen.generate(unfold_model, (T,));
-
+#
 choices = get_choices(trace)
 retval = get_retval(trace)
-
-locations = Array{Coordinate}(undef, T)
-locations[1] = retval[1].current_location #starting value
-for t = 2:T
-    locations[t] = retval[2][t].current_location
-end
-
-println(locations)
-
+#
+# locations = Array{Coordinate}(undef, T)
+# locations[1] = retval[1].current_location #starting value
+# for t = 2:T
+#     locations[t] = retval[2][t].current_location
+# end
+#
+# println(locations)
+#
 locations = Array{Coordinate}(undef, T)
 movement_times = Array{Float64}(undef, T)
 distracted = Array{Float64}(undef, T)
@@ -56,22 +56,22 @@ end
 println(locations) #doesn't have starting value
 println(movement_times)
 println(distracted)
-
-
-# #see if I can infer x and y from deterministic thing
-num_particles = 100
-unfold_pf_traces = unfold_particle_filter(num_particles, locations, movement_times, num_particles)
 #
 #
-#how to access values in the traces from the particle filter.
-inferred_distracted = zeros(T)
-for i = 1:num_particles
-    for t = 1:T
-        inferred_distracted[t] = inferred_distracted[t] + unfold_pf_traces[i][:chain => t => :find_best => :distracted]
-    end
-end
-inferred_distracted = inferred_distracted./num_particles
-MSE = sum((distracted .- inferred_distracted).^2)
+# # #see if I can infer x and y from deterministic thing
+# num_particles = 100
+# unfold_pf_traces = unfold_particle_filter(num_particles, locations, movement_times, num_particles)
+# #
+# #
+# #how to access values in the traces from the particle filter.
+# inferred_distracted = zeros(T)
+# for i = 1:num_particles
+#     for t = 1:T
+#         inferred_distracted[t] = inferred_distracted[t] + unfold_pf_traces[i][:chain => t => :find_best => :distracted]
+#     end
+# end
+# inferred_distracted = inferred_distracted./num_particles
+# MSE = sum((distracted .- inferred_distracted).^2)
 
 
 #################################################################################
--- Model/Attempt1/helper_functions.jl
+++ Model/Attempt1/helper_functions.jl
@@ -96,6 +96,148 @@ end
     return next_node, way_so_far
 end
 
+@gen function find_best_IDDLS(current_node::Node, node_matrix::Matrix{Node}, way_so_far::Array{Coordinate}, speed_of_thought_factor::Float64)
+    println(current_node.location)
+    #println("current_node ", current_node)
+    candidates = current_node.viable_children;
+    #evaluations will heuristic's the values for each path
+    evaluations = Array{Float64, 1}(undef, length(candidates))
+    #counter for number of times DLS is called. it's value will be related to max_depth
+    global counter = 0.0;
+    max_depth = @trace(uniform_discrete(20,20), :max_depth)
+    min_depth = @trace(uniform_discrete(2,2), :min_depth)
+    #always do a search with min_depth
+    for i = 1:length(candidates)
+        #give DLS a fake copy of the node_matrix and all the nodes. don't actually want the node_matrix changed.
+        dpcpy_matrix = deepcopy(node_matrix)
+        candidate_node = dpcpy_matrix[candidates[i].x, candidates[i].y]
+        parent_node = dpcpy_matrix[current_node.location.x, current_node.location.y]
+        update(candidate_node, parent_node)
+        #evaluations[i] = DLS_wrapper(candidate_node, dpcpy_matrix, 1)
+        evaluations[i] = DLS(candidate_node, dpcpy_matrix, min_depth)
+    end
+    #while the goal hasn't been found and there are more than one good option
+    println("evaluations ", evaluations)
+    depth = min_depth
+    while (!(0 in evaluations) && sum(evaluations .< Inf) > 1 && depth < max_depth)
+        println("in while loop")
+        depth = depth + 1
+        for i = 1:length(candidates)
+            #give DLS a fake copy of the node_matrix and all the nodes. don't actually want the node_matrix changed.
+            dpcpy_matrix = deepcopy(node_matrix)
+            candidate_node = dpcpy_matrix[candidates[i].x, candidates[i].y]
+            parent_node = dpcpy_matrix[current_node.location.x, current_node.location.y]
+            update(candidate_node, parent_node)
+            #evaluations[i] = DLS_wrapper(candidate_node, dpcpy_matrix, 1)
+            evaluations[i] = DLS(candidate_node, dpcpy_matrix, depth)
+        end
+    end
+
+
+    println("depth", depth)
+    println("counter", counter)
+    ############################################################################
+    #computation stuff
+    #computation time has mean counter, sd 5, min 0, max 100
+    distracted = @trace(bernoulli(0.05), :distracted)
+    #distraction adds, on average, 100 to computation time. also increases sd
+    sd = 0.1
+    computation_time = @trace(trunc_normal(speed_of_thought_factor*counter + distracted*300, sd + distracted*10, 0.0, 100000.0), :computation_time)
+    movement_time = @trace(trunc_normal(maximum([computation_time, 10.0]), sd, 0.0, 100000.0), :movement_time)
+
+    println("distracted ", distracted)
+    println("computation_time ", computation_time)
+    println("movement_time ", movement_time)
+    ############################################################################
+    val, index = findmin(evaluations)
+    #if all deadends
+    if val == Inf
+        #remove the children
+        for i = 1:length(candidates)
+            candidate = candidates[1]#because candidates gets shorter every time
+            candidate_node = node_matrix[candidate.x, candidate.y]
+            update(candidate_node, current_node)
+        end
+        next_node, way_so_far = backtrack(current_node.location, node_matrix, way_so_far);
+    else
+        #update stuff
+        next_node_coor = candidates[index] #best option
+        next_node = node_matrix[next_node_coor.x, next_node_coor.y]
+        update(current_node, next_node)
+        push!(way_so_far, next_node_coor)
+    end
+    return next_node, way_so_far
+end
+
+@gen function find_best_min_max(current_node::Node, node_matrix::Matrix{Node}, way_so_far::Array{Coordinate}, speed_of_thought_factor::Float64)
+    println(current_node.location)
+    #println("current_node ", current_node)
+    candidates = current_node.viable_children;
+    #evaluations will heuristic's the values for each path
+    evaluations = Array{Float64, 1}(undef, length(candidates))
+    #counter for number of times DLS is called. it's value will be related to max_depth
+    global counter = 0.0;
+    max_depth = @trace(uniform_discrete(20,20), :max_depth)
+    min_depth = @trace(uniform_discrete(2,2), :min_depth)
+    #always do a search with min_depth
+    for i = 1:length(candidates)
+        #give DLS a fake copy of the node_matrix and all the nodes. don't actually want the node_matrix changed.
+        dpcpy_matrix = deepcopy(node_matrix)
+        candidate_node = dpcpy_matrix[candidates[i].x, candidates[i].y]
+        parent_node = dpcpy_matrix[current_node.location.x, current_node.location.y]
+        update(candidate_node, parent_node)
+        #evaluations[i] = DLS_wrapper(candidate_node, dpcpy_matrix, 1)
+        evaluations[i] = DLS(candidate_node, dpcpy_matrix, min_depth)
+    end
+    #while the goal hasn't been found and there are more than one good option
+    println("evaluations ", evaluations)
+    if (!(0 in evaluations) && sum(evaluations .< Inf) > 1)
+        println("in if")
+        for i = 1:length(candidates)
+            #give DLS a fake copy of the node_matrix and all the nodes. don't actually want the node_matrix changed.
+            dpcpy_matrix = deepcopy(node_matrix)
+            candidate_node = dpcpy_matrix[candidates[i].x, candidates[i].y]
+            parent_node = dpcpy_matrix[current_node.location.x, current_node.location.y]
+            update(candidate_node, parent_node)
+            #evaluations[i] = DLS_wrapper(candidate_node, dpcpy_matrix, 1)
+            evaluations[i] = DLS(candidate_node, dpcpy_matrix, max_depth)
+        end
+    end
+
+    println("counter", counter)
+    ############################################################################
+    #computation stuff
+    #computation time has mean counter, sd 5, min 0, max 100
+    distracted = @trace(bernoulli(0.05), :distracted)
+    #distraction adds, on average, 100 to computation time. also increases sd
+    sd = 0.1
+    computation_time = @trace(trunc_normal(speed_of_thought_factor*counter + distracted*300, sd + distracted*10, 0.0, 100000.0), :computation_time)
+    movement_time = @trace(trunc_normal(maximum([computation_time, 10.0]), sd, 0.0, 100000.0), :movement_time)
+
+    println("distracted ", distracted)
+    println("computation_time ", computation_time)
+    println("movement_time ", movement_time)
+    ############################################################################
+    val, index = findmin(evaluations)
+    #if all deadends
+    if val == Inf
+        #remove the children
+        for i = 1:length(candidates)
+            candidate = candidates[1]#because candidates gets shorter every time
+            candidate_node = node_matrix[candidate.x, candidate.y]
+            update(candidate_node, current_node)
+        end
+        next_node, way_so_far = backtrack(current_node.location, node_matrix, way_so_far);
+    else
+        #update stuff
+        next_node_coor = candidates[index] #best option
+        next_node = node_matrix[next_node_coor.x, next_node_coor.y]
+        update(current_node, next_node)
+        push!(way_so_far, next_node_coor)
+    end
+    return next_node, way_so_far
+end
+
 #implements a DLS that returns the best value possible from a given starting point
 function DLS(current_node::Node, node_matrix::Matrix{Node}, depth::Int64)
     global counter = counter + 1;
